66e03188087db836646430161883d9f47076a7bd
Juraj Skripsky <juraj.skripsky@gmail.com>
2023-10-29 17:50:11 +0100

    Add types from import of reservations and players (unused for now).

--- src/Types.fs
+++ src/Types.fs
@@ -14,3 +14,28 @@ type Blocking = {
     StartEnd: (TimeOnly * TimeOnly) option
     Note: string
 }
+
+type PlayerId = PlayerId of int
+type ReservationOwner = Player | Club
+
+type Reservation = {
+    Players: PlayerId list
+    Court: CourtNo
+    Date: DateOnly
+    StartEnd: (TimeOnly * TimeOnly) option
+    BallMachine: bool
+    Owner: ReservationOwner
+}
+
+type Validated<'T> = {
+    IsValidated: bool
+    Value: 'T
+}
+
+type Player = {
+    Id: PlayerId
+    LastName: string
+    FirstName: string
+    Email: Validated<string> option
+    Phone: Validated<string> option
+}
